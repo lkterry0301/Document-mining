93794d2ffa70477fe0c61ecd9e534cfe73f3351f
lkterry0301 <terryliu0301@gmail.com>
2016-01-28 19:54:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -11,31 +11,6 @@ Vocabulary = {}
 Freq = []
 #print soup.prettify()
 
-for doc in soup.find_all("reuters"):
-	id = doc.get("newid")
-	#print id
-	freq = {}
-	if doc.find("body") is not None:
-		words = nltk.word_tokenize(doc.find("body").string)
-		for word in words:
-			word = word.lower()
-			word = stemmer.stem(word) #get words stemmed
-			if word not in freq:
-				if word not in Vocabulary:
-					Vocabulary[word] = 1
-				else:
-					Vocabulary[word] += 1
-				freq[word] = 1
-			else:
-				freq[word] += 1
-	Freq.append(freq)
-
-for key in Vocabulary.keys():
-	if Vocabulary[key] == 1:
-		del Vocabulary[key]
-	else:
-		print key
-
 def get_word_frequency(soup):
     currentFreq=[]
     for doc in soup.find_all("reuters"):
