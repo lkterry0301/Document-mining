ff36631a6d0e196b60528483d81d222910098a84
lkterry0301 <terryliu0301@gmail.com>
2016-01-28 19:45:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,35 +3,44 @@
 #Another thing we forgot to add is the class labels as provided in the TOPICS and PLACES tabs of each article
 
 import nltk
-from nltk import stem
+from os import listdir
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("data/reut2-000.sgm"),"html.parser")
-stemmer = stem.porter.PorterStemmer() 
+stemmer = nltk.stem.porter.PorterStemmer() 
 Vocabulary = {}
 Freq = []
 #print soup.prettify()
 
+def get_word_frequency(soup):
+    currentFreq=[]
+    for doc in soup.find_all("reuters"):
+                id = doc.get("newid")
+                #print id
+                freq = {}
+                if doc.find("body") is not None:
+                        words = nltk.word_tokenize(doc.find("body").string)
+                        for word in words:
+                                word = word.lower()
+                                word = stemmer.stem(word) #get words stemmed
+                                if word not in freq:
+                                        if word not in Vocabulary:
+                                                Vocabulary[word] = 1
+                                        else:
+                                                Vocabulary[word] += 1
+                                        freq[word] = 1
+                                else:
+                                        freq[word] += 1
+    currentFreq.append(freq)
+    return currentFreq
+    
+#ask for directory for dataset
+file_path=raw_input("Please enter the directory of reuters dataset: ") 
 
-for doc in soup.find_all("reuters"):
-	id = doc.get("newid")
-	#print id
-	freq = {}
-	if doc.find("body") is not None:
-		words = nltk.word_tokenize(doc.find("body").string)
-		for word in words:
-			word = word.lower()
-			word = stemmer.stem(word) #get words stemmed
-			if word not in freq:
-				if word not in Vocabulary:
-					Vocabulary[word] = 1
-				else:
-					Vocabulary[word] += 1
-				freq[word] = 1
-			else:
-				freq[word] += 1
-	Freq.append(freq)
-
+#iterate through all files in the given directory
+for file in listdir(file_path):
+    soup = BeautifulSoup(open(file_path+file),"html.parser")
+    Freq.append(get_word_frequency(soup))
+    
 for key in Vocabulary:
 	print key
 
