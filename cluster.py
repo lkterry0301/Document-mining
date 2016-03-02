b87ea2196ce490bbec65609085182437db324c4a
Kun Liu <terryliu0301@gmail.com>
2016-03-01 19:56:57 -0500

    modify DBSCAN_clustering

--- cluster.py
+++ cluster.py
@@ -10,15 +10,12 @@ def kmeans_clustering(n_clusters,word_vector):
   
 #Hierarchical clustering function
 def hierarchical_clustering(n_clusters,word_vector,metric,link):
-  #affinity is metric used. Can be "euclidean", "l1", "l2", "manhattan", "cosine", or "precomputed". If linkage is "ward", only "euclidean" is accepted.
-  #linkage can be {"ward", "complete", "average"}
   model=AgglomerativeClustering(n_clusters=n_clusters,affinity=metric, linkage=link) 
   prediction=model.fit_predict(word_vector)
   return prediction
   
 #DBSCAN clustering function
 def DBSCAN_clustering(eps,sample_size, word_vector,metric):
-  #metrican be "euclidean", "l1", "l2", "manhattan", "cosine", or "precomputed". If linkage is "ward", only "euclidean" is accepted.
   model= DBSCAN(eps=eps,min_samples=sample_size, metric=metric) 
   prediction=model.fit_predict(word_vector)
   return prediction
