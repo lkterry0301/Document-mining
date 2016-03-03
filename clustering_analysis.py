d0b3fc45b8d707dfbcdf7ef9d752129c156f206d
Kun Liu <terryliu0301@gmail.com>
2016-03-02 22:40:48 -0500

    update output

--- clustering_analysis.py
+++ clustering_analysis.py
@@ -7,25 +7,23 @@ parser.add_option("-f", "--file", dest="in_file",
 parser.add_option("-o", "--output", dest="out_file",
 		  help="the output matrix file", metavar="FILE")
 parser.add_option("-m", "--metric", dest="metric",
-		  metavar="[Euclidean|Other]",
-		 default="cosine", help="Type of similarity")
+		  metavar="[Euclidean|Manhattan]",
+		 default="euclidean", help="Type of similarity")
 parser.add_option("-a", "--algorithm", dest="algorithm",
-		  metavar="[DBSCAN|Other]",
+		  metavar="[DBSCAN|Hierarchical]",
 		  default="DBSCAN")
 parser.add_option("-e", "--eps", dest="epsilon", metavar="<Epsilon>",
 		  type="float", default=0.5)
 parser.add_option("-M", "--min-sample", dest="min_sample", metavar="<Min samples>",
-		  type="int", default=5)
+		  type="int", default=5) 
 parser.add_option("-t", "--test", dest="small_data",
 		  action="store_true", default=False)
 parser.add_option("-k", dest="cluster", metavar="<Cluster>", type="int",
-		  default=10)
+		  default=2)
+parser.add_option("-l", dest="linkage", metavar="[Ward|Average|Complete]",
+		  default="ward")
+
 (options, args) = parser.parse_args()
-
-
-import time
-start_time = time.time()
-
 
 #open vector file
 vector_file = open(options.in_file, 'r')
@@ -41,10 +39,13 @@ cdim = int(info.readline())
 
 
 if options.small_data:
-	rdim = 5000;
-	print 'small test set selected, only loading 5000 rows'
+	rdim = 1000;
+	print 'small test set selected, only loading 1000 rows'
 
 S = lil_matrix((rdim, cdim))
+
+import time
+start_time = time.time()
 
 print "Reading vectors... ",
 sys.stdout.flush()
@@ -67,32 +68,70 @@ print "done"
 S = S.tocsr()
 
 data_process_time= time.time()  - start_time
-print "Data processing took "+str(data_process_time)+" seconds"
 
 #choose cluster method
+print ("Clustering... ")
 import cluster
 prediction = []
 
-'''
+
 if options.algorithm.lower()=="dbscan":
-	prediction=cluster.DBSCAN_clustering(options.epsilon, options.min_sample,S,options.metric.lower())
-elif options.algorithm.lower()=="kmeans":
-	prediction=cluster.kmeans_clustering(options.cluster, S)
+    print ("DBSCAN")
+    eps=options.epsilon
+    min_sample=options.min_sample
+    metric=options.metric.lower()
+    print ("eps= "+str(eps)+", min_sample= "+str(min_sample)+", metric= "+metric)
+    prediction=cluster.DBSCAN_clustering(eps,min_sample,S,metric)
+    S=S.toarray()
+elif options.algorithm.lower()=="hierarchical":
+    S=S.toarray()
+    print ("Hierarchical")
+    n_clusters=options.cluster
+    metric=options.metric.lower()
+    link=options.linkage.lower()
+    print ("num_cluster= "+str(n_clusters)+", metric= "+metric+", linkage= "+link)
+    prediction=cluster.hierarchical_clustering(n_clusters,S,metric,link)
+
+'''
+print ("DBSCAN")
+eps=0.5
+min_sample=10
+metric='manhattan'#manhattan
+print ("eps= "+str(eps)+", min_sample= "+str(min_sample)+", metric= "+metric)
+prediction=cluster.DBSCAN_clustering(eps,min_sample,S,metric)
+S=S.toarray()
 '''
 
-prediction=cluster.DBSCAN_clustering(0.5,5,S,'euclidean')
-#prediction=cluster.hierarchical_clustering(100,S.toarray(),'euclidean','ward')
+'''
+S=S.toarray()
+print ("Hierarchical")
+n_clusters=500
+metric='euclidean'#manhattan
+link='ward'
+print ("num_cluster= "+str(n_clusters)+", metric= "+metric+", linkage= "+link)
+prediction=cluster.hierarchical_clustering(n_clusters,S,metric,link)
+'''
+
+print "done"
 
 clustering_time= time.time()  - start_time - data_process_time
-print "Clustering took "+str(clustering_time)+" seconds"
 
 #evaluate result
 quality_start_time=time.time()
 import quality
-quality.quality_evaluation(prediction, S.toarray(), label)
+quality.quality_evaluation(prediction, S, label)
 
 quality_time=time.time()-quality_start_time
 total_time=time.time()-start_time
+
+print ""
+print "Running time:"
+print "Data processing time: "+str(data_process_time)+" seconds"
+print "Clustering time: "+str(clustering_time)+" seconds"
+print "Quality evaluation time: "+str(quality_time)+" seconds"
+print "Total running time: "+str(total_time)+" seconds"
+print ""
+
 
 #print len(prediction)
 #print cluster result
@@ -106,7 +145,3 @@ for item in prediction:
 
 for key in hist:
 	print key," ", hist[key]
-
-print "Quality evaluation time: "+str(quality_time)+" seconds"
-print "Total running time: "+str(total_time)+" seconds"
-print ""
